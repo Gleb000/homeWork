618413f665a7b1a8ac09e8b247bf60c049daf57e
Gleb <glebfox3@gmail.com>
2020-02-26 19:52:01 +0300

    First Commit

---
+++ homework1/src/by/Fokin/homework13_1.java
@@ -0,0 +1,26 @@
+package by.Fokin;
+
+import java.util.Scanner;
+
+public class homework13_1 {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Введите число");
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            int g = 0;
+            for (int i = 2; i < a; i += 1) {
+                if (a % i == 0) {
+                    System.out.println("Число не простое");
+                    g = 1;
+                    break;
+                }
+                if (i == a - 1) {
+                    System.out.println("Число простое");
+                }
+            }
+            if(g == 1)
+                break;
+        }
+    }
+}
