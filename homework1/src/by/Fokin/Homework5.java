f6582456847a7d7f458ec8699f8a114d3bf3bdcb
Gleb <glebfox3@gmail.com>
2020-04-07 21:52:58 +0300

    Fixing error in TaskX

--- homework1/src/by/Fokin/Homework5.java
+++ homework1/src/by/Fokin/Homework5.java
@@ -2,14 +2,15 @@ package by.Fokin;
 
 import java.util.Scanner;
 
-public class homework5 {
+public class Homework5 {
 
     public static void main(String[] args) {
         System.out.println("Введите сумму денег");
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int g = a % 10;
-        if (g == 0 || g == 5 || g == 6 || g == 7 || g == 8 || g == 9) {
+        if (g == 0 || g == 5 || g == 6 || g == 7 || g == 8 || g == 9 ||
+                a == 11 || a == 12 || a == 13 || a == 14) {
             System.out.println(a + " рублей");
         }
         else if (g == 1) {
